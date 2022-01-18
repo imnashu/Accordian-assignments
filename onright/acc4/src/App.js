cb18a51ab5596c95d3eaea00421bd0080dda625c
imnashu07 <imnashu07@gmail.com>
2022-01-18 23:52:04 +0530

    on right

---
+++ onright/acc4/src/App.js
@@ -0,0 +1,18 @@
+import MultiAcc from './MultiAcc';
+import './style.css'
+import { accData } from './data/data';
+
+function App() {
+  return (
+    <div className='viewport'>
+      <h1>Accordian 4th assignment</h1>
+      <div className="Maindiv">
+        {accData.map(({ title, content }) => (
+          <MultiAcc title={title} content={content}/>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
