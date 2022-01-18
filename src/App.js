bc2ca90902ba80c9ee3bea6f8287754a30cd54f9
imnashu07 <imnashu07@gmail.com>
2022-01-18 23:42:27 +0530

    Acc3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { accData } from './data/data';
 function App() {
   return (
     <div className='viewport'>
-      <h1>Accordian 4th assignment</h1>
+      <h1>Accordian 3RD assignment</h1>
       <div className="Maindiv">
         {accData.map(({ title, content }) => (
           <MultiAcc title={title} content={content}/>
